d053a32b88a98440006e1fb4c98b76bf37664e06
Sai Teja Gangisetti <143573794+sai220284@users.noreply.github.com>
2023-10-25 11:46:27 -0500

    users

--- routes/users.js
+++ routes/users.js
@@ -5,17 +5,12 @@ let sum = 0;
 let more = 0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  router.get('/', (req, res) => {
+  
     more += 1;
     sum += more;
   
-    // ... your other code ...
-  
-    // Send the response with the updated sum
     res.send(`Sum is: ${sum}`);
   });
-  
-  res.send('respond with a resource');
-});
+
 
 module.exports = router;
